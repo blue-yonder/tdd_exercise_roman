c1aec9e6d73ae47930e09b81e706be8934dea258
Michael König <michael.koenig@blue-yonder.com>
2016-11-18 13:22:42 +0100

    Add project scaffold with pytest support

---
+++ tests/test_roman.py
@@ -0,0 +1,17 @@
+from roman import arabic_to_roman
+
+"""
+Use this file to test-drive the implementation of an `arabic_to_roman()`
+function. Repeat the following steps until you have solved the task:
+
+1. Write a _minimal_ failing test.
+2. Pass the test with a _minimal_ change to your production code.
+3. Improve both tests and production code with refactorings.
+
+Heed the word "minimal". You can fail a test with a trivial thing such
+as importing a function that is not yet defined. A trivial fix to your
+production code could be to create a simple stub that does nothing at all.
+
+Execute the tests with `python setup.py test`. This test file will fail
+when it is freshly checked out.
+"""
