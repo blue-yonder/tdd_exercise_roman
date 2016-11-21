9b0646b45832bbc592db4815c5c151ec4d54794b
Michael König <michael.koenig@blue-yonder.com>
2016-11-21 23:33:24 +0100

    Fix 4000 case

--- roman/__init__.py
+++ roman/__init__.py
@@ -1,5 +1,5 @@
 def arabic_to_roman(arabic):
-    if arabic > 0:
+    if arabic > 0 and arabic < 4000:
         return "I" * arabic
     else:
         raise RuntimeError("Arabic number must be positive and less than 4000")
