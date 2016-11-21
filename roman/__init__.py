9be0832c8d8268a2f99fa6a1899a37031c2d6227
Michael König <michael.koenig@blue-yonder.com>
2016-11-21 23:47:32 +0100

    Extract code for translating ones

--- roman/__init__.py
+++ roman/__init__.py
@@ -1,15 +1,19 @@
+def _ones_to_roman(ones):
+    if ones == 4:
+        return "IV"
+    elif ones == 9:
+        return "IX"
+    else:
+        if ones >= 5:
+            roman = "V"
+            ones -= 5
+        else:
+            roman = ""
+        return roman + "I" * ones
+
+
 def arabic_to_roman(arabic):
     if arabic > 0 and arabic < 4000:
-        if arabic == 4:
-            return "IV"
-        elif arabic == 9:
-            return "IX"
-        else:
-            if arabic >= 5:
-                roman = "V"
-                arabic -= 5
-            else:
-                roman = ""
-            return roman + "I" * arabic
+        return _ones_to_roman(arabic)
     else:
         raise RuntimeError("Arabic number must be positive and less than 4000")
