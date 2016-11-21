c3892b79e08b4066a1736fb59eb0c4be081b3026
Michael König <michael.koenig@blue-yonder.com>
2016-11-21 23:44:04 +0100

    FInd simplified solution for 5-8

--- roman/__init__.py
+++ roman/__init__.py
@@ -2,11 +2,12 @@ def arabic_to_roman(arabic):
     if arabic > 0 and arabic < 4000:
         if arabic == 4:
             return "IV"
-        elif arabic == 5:
-            return "V"
-        elif arabic == 6:
-            return "VI"
         else:
-            return "I" * arabic
+            if arabic >= 5:
+                roman = "V"
+                arabic -= 5
+            else:
+                roman = ""
+            return roman + "I" * arabic
     else:
         raise RuntimeError("Arabic number must be positive and less than 4000")
