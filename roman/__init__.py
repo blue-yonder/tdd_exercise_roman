643ba0a1b89ff200214684ce39e6e385776f33c9
Michael König <michael.koenig@blue-yonder.com>
2016-11-22 00:00:10 +0100

    Extract function to handle digit

--- roman/__init__.py
+++ roman/__init__.py
@@ -1,36 +1,22 @@
-def _ones_to_roman(ones):
-    if ones == 4:
-        return "IV"
-    elif ones == 9:
-        return "IX"
+def _digit_to_roman(digit, one_symbol, five_symbol, ten_symbol):
+    if digit == 4:
+        return one_symbol + five_symbol
+    elif digit == 9:
+        return one_symbol + ten_symbol
     else:
-        if ones >= 5:
-            roman = "V"
-            ones -= 5
+        if digit >= 5:
+            roman = five_symbol
+            digit -= 5
         else:
             roman = ""
-        return roman + "I" * ones
-
-
-def _tens_to_roman(tens):
-    if tens == 4:
-        return "XL"
-    elif tens == 9:
-        return "XC"
-    else:
-        if tens >= 5:
-            roman = "L"
-            tens -= 5
-        else:
-            roman = ""
-        return roman + "X" * tens
+        return roman + one_symbol * digit
 
 
 def arabic_to_roman(arabic):
     if arabic > 0 and arabic < 4000:
         if arabic < 10:
-            return _ones_to_roman(arabic)
+            return _digit_to_roman(arabic, "I", "V", "X")
         else:
-            return _tens_to_roman(arabic / 10)
+            return _digit_to_roman(arabic / 10, "X", "L", "C")
     else:
         raise RuntimeError("Arabic number must be positive and less than 4000")
