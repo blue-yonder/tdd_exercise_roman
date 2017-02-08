5d9c572289e685e0f2b32c122151197aa1ea7790
Michael König <michael.koenig@blue-yonder.com>
2017-02-08 16:27:53 +0100

    Pass test for 5

--- roman/src/roman.cpp
+++ roman/src/roman.cpp
@@ -6,6 +6,8 @@ std::string arabic_to_roman(int arabic)
     if ((arabic > 0) and (arabic < 4000)) {
         if (arabic == 4) {
             return "IV";
+        } if (arabic == 5) {
+            return "V";
         } else {
             std::string roman("");
             for (int i = 0; i != arabic; ++i) {
