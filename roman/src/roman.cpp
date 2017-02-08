d39d2338588b85a66274fe8732c75e25d64f2879
Michael König <michael.koenig@blue-yonder.com>
2017-02-08 16:31:31 +0100

    Pass tests for 7 and 8

--- roman/src/roman.cpp
+++ roman/src/roman.cpp
@@ -6,17 +6,20 @@ std::string arabic_to_roman(int arabic)
     if ((arabic > 0) and (arabic < 4000)) {
         if (arabic == 4) {
             return "IV";
-        } if (arabic == 5) {
-            return "V";
-        } if (arabic == 6) {
-            return "VI";
-        } else {
-            std::string roman("");
-            for (int i = 0; i != arabic; ++i) {
-                roman += "I";
-            }
-            return roman;
         }
+
+        std::string roman("");
+
+        if (arabic >= 5) {
+            roman += "V";
+            arabic -= 5;
+        }
+
+        for (int i = 0; i != arabic; ++i) {
+            roman += "I";
+        }
+
+        return roman;
     } else {
         throw std::domain_error("");
     }
